cff5232380789ff5eb3df157539a3a2f56ee1f54
ByBlener <blener@hotmail.com>
2020-05-05 18:10:18 -0300

    using dotnet restore

--- fake_build.fsx
+++ fake_build.fsx
@@ -28,7 +28,7 @@ Target.create "EnsureCleanFolders" (fun _ ->
 )
 
 Target.create "RestorePackages" (fun _ ->
-    NuGet.Restore.RestorePackages()
+    runDotNet "restore" "./"
     Trace.trace @"---Packages Restored---"
 )
 
